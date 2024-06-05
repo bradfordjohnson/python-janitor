c87ca366646d697d8c56b1fad65f8c3d5fe92603
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-06-05 00:04:44 -0400

    added requirements and expanded tests

--- tests/test_helpers_normalize_string.py
+++ tests/test_helpers_normalize_string.py
@@ -6,5 +6,17 @@ import re
 def test_removes_non_alphanumeric_characters():
     assert normalize_string("Hello, World!") == "hello_world"
 
+def test_multiple_spaces():
+    assert normalize_string("Hello,  World!") == "hello_world"
+
+def test_numbers_in_front_of_string():
+    assert normalize_string("123abc") == "123_abc"
+
+def test_numbers_behind_string():
+    assert normalize_string("abc123") == "abc_123"
+    
+def test_numbers_in_middle_of_string():
+    assert normalize_string("ab123c") == "ab_123_c"
+    
 if __name__ == "__main__":
     pytest.main()
