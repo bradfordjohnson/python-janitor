0c22b5df89ca5ef44b3af3e357267604c6406048
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2024-06-05 00:05:24 -0400

    formatting update

--- functions/helpers.py
+++ functions/helpers.py
@@ -1,11 +1,12 @@
 import re
 
+
 def normalize_string(s):
-    cleaned = re.sub(r'[^\w\s]', '', s.lower())
-    
-    cleaned = re.sub(r'([a-zA-Z])(\d)', r'\1_\2', cleaned)
-    cleaned = re.sub(r'(\d)([a-zA-Z])', r'\1_\2', cleaned)
-    
-    result = re.sub(r'\s+|_{2,}', '_', cleaned)
-    
+    cleaned = re.sub(r"[^\w\s]", "", s.lower())
+
+    cleaned = re.sub(r"([a-zA-Z])(\d)", r"\1_\2", cleaned)
+    cleaned = re.sub(r"(\d)([a-zA-Z])", r"\1_\2", cleaned)
+
+    result = re.sub(r"\s+|_{2,}", "_", cleaned)
+
     return result
